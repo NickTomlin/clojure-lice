4f1f128caf33841bb1ae46120ed9229775abfc2b
Nick Tomlin <nick.tomlin@gmail.com>
2017-10-22 15:10:35 -0500

    feat: Actually fetch licenses

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject lice "0.1.0-SNAPSHOT"
   :main lice.core
-  :description "FIXME: write description"
+  :description "A clojure app providing programattic access to choosealicense.org"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.12.1"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :ring {:handler lice.core/handler :port 8000})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [compojure "1.6.0"]]
+  :ring {:handler lice.core/app :port 8001})
--- src/lice/core.clj
+++ src/lice/core.clj
@@ -1,8 +1,34 @@
-(ns lice.core)
+(ns lice.core
+  (:require [net.cgrand.enlive-html :as html]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
 
-(def names ["Evan", "Josh", "Hurley (we would never call him 'Sean')", "Paul", "Rohit"])
+(defn extract-text [license-response]
+  (html/text (first (html/select  license-response [:#license-text]))))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "Hello " (rand-nth names))})
+(defn request-license [license-name]
+  (html/html-resource
+    (java.net.URL. (str "https://choosealicense.com/licenses/" license-name "/"))))
+
+(defn fetch-license [license-name]
+  (extract-text (request-license license-name)))
+
+(defn exception-handler
+  [handler]
+  (fn [request]
+    (try
+      (handler request)
+      (catch Exception e
+        (println "err" e)
+        {:status 400 :body "Whoopsie. That didn't work"}))))
+
+(defroutes app-routes
+  (GET "/" [] "Hello!")
+  (GET "/licenses/:name" [name] (fetch-license name))
+  (route/not-found "That was not found...."))
+
+(def app
+  (-> (handler/api app-routes)
+      exception-handler))
